63b43fad82f8b32e7ff8334bb6060296224b7668
Reggeditt <tkodjosarso@gmail.com>
2023-01-02 14:39:33 +0000

    fixed eslint issues

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-const menuIconElement = document.getElementById("menu-icon");
-const menuElement = document.getElementById("menu");
-const closeMenuElement = document.getElementById("close-menu-icon");
-const navElement = document.getElementById("nav");
-const logoElement = document.getElementById("logo");
-const menuLinkPortfolioElement = document.getElementById("menu-link-portfolio");
-const menuLinkAboutElement = document.getElementById("menu-link-about");
-const menuLinkContactElement = document.getElementById("menu-link-contact");
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+const closeMenuElement = document.getElementById('close-menu-icon');
+const navElement = document.getElementById('nav');
+const logoElement = document.getElementById('logo');
+const menuLinkPortfolioElement = document.getElementById('menu-link-portfolio');
+const menuLinkAboutElement = document.getElementById('menu-link-about');
+const menuLinkContactElement = document.getElementById('menu-link-contact');
 
-menuIconElement.addEventListener("click", () => {
-  menuElement.style.display = "flex";
-  menuIconElement.style.display = "none";
-  logoElement.style.display = "none";
-  navElement.style.width = "100%";
-  navElement.style.height = "100vh";
-  navElement.style.paddingTop = "20px";
-  navElement.style.paddingBottom = "75%";
+menuIconElement.addEventListener('click', () => {
+  menuElement.style.display = 'flex';
+  menuIconElement.style.display = 'none';
+  logoElement.style.display = 'none';
+  navElement.style.width = '100%';
+  navElement.style.height = '100vh';
+  navElement.style.paddingTop = '20px';
+  navElement.style.paddingBottom = '75%';
 });
 
-closeMenuElement.addEventListener("click", () => {
-  menuElement.style.display = "none";
-  menuIconElement.style.display = "inline-block";
-  logoElement.style.display = "inline-block";
-  navElement.style.width = "fit-content";
-  navElement.style.height = "18px";
-  navElement.style.paddingTop = "0";
-  navElement.style.paddingBottom = "0";
+closeMenuElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
 });
 
-menuLinkPortfolioElement.addEventListener("click", () => {
-  menuElement.style.display = "none";
-  menuIconElement.style.display = "inline-block";
-  logoElement.style.display = "inline-block";
-  navElement.style.width = "fit-content";
-  navElement.style.height = "18px";
-  navElement.style.paddingTop = "0";
-  navElement.style.paddingBottom = "0";
+menuLinkPortfolioElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
 });
 
-menuLinkAboutElement.addEventListener("click", () => {
-  menuElement.style.display = "none";
-  menuIconElement.style.display = "inline-block";
-  logoElement.style.display = "inline-block";
-  navElement.style.width = "fit-content";
-  navElement.style.height = "18px";
-  navElement.style.paddingTop = "0";
-  navElement.style.paddingBottom = "0";
+menuLinkAboutElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
 });
 
-menuLinkContactElement.addEventListener("click", () => {
-  menuElement.style.display = "none";
-  menuIconElement.style.display = "inline-block";
-  logoElement.style.display = "inline-block";
-  navElement.style.width = "fit-content";
-  navElement.style.height = "18px";
-  navElement.style.paddingTop = "0";
-  navElement.style.paddingBottom = "0";
+menuLinkContactElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
 });
