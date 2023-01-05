f394e09258804b3fb5dedf75a5d33b46dd7383f5
Reggeditt <tkodjosarso@gmail.com>
2023-01-05 15:01:51 +0000

    added styling for mobile and desktop popup

--- index.js
+++ index.js
@@ -8,33 +8,92 @@ const menuLinkPortfolioElement = document.getElementById('menu-link-portfolio');
 const menuLinkAboutElement = document.getElementById('menu-link-about');
 const menuLinkContactElement = document.getElementById('menu-link-contact');
 
+// menu icon code starts here
+menuIconElement.addEventListener('click', () => {
+  menuElement.style.display = 'flex';
+  menuIconElement.style.display = 'none';
+  logoElement.style.display = 'none';
+  navElement.style.width = '100%';
+  navElement.style.height = '100vh';
+  navElement.style.paddingTop = '20px';
+  navElement.style.paddingBottom = '75%';
+});
+
+closeMenuElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
+});
+
+menuLinkPortfolioElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
+menuLinkAboutElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
+menuLinkContactElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
 /* modal popup window elements */
 const projectButtonElements = document.getElementsByClassName('pjt-button');
 const projectModalContentElements = document.getElementsByClassName('project-modal-content');
 const modalContainerElement = document.getElementById('modal-container');
+const closeModalIconElement = document.getElementById('close-modal-icon');
+
+const popupProjectName = document.createElement('h2');
+popupProjectName.className = 'modal-heading header-text';
+const popupStack1 = document.createElement('span');
+popupStack1.className = 'modal-stacks';
+const popupStack2 = document.createElement('span');
+popupStack2.className = 'modal-stacks';
+const popupStack3 = document.createElement('span');
+popupStack3.className = 'modal-stacks';
+const div1 = document.createElement('div');
+div1.className = 'divide';
+const div2 = document.createElement('div');
+div2.className = 'illustration works-img';
+const description = document.createElement('p');
+description.className = 'modal-description description2-text';
+const popupButton1 = document.createElement('a');
+popupButton1.className = 'main-btn see-live';
+popupButton1.href = 'https://www.google.com/';
+const popupButton2 = document.createElement('a');
+popupButton2.className = 'main-btn see-source';
+popupButton2.href = 'https://github.com/Reggeditt/Portfolio/';
 
 for (let i = 0; i < projectButtonElements.length; i++) {
   projectButtonElements[i].addEventListener('click', () => {
-    const popupProjectName = document.createElement('h2');
-    popupProjectName.className = 'modal-heading header-text';
-    const popupStack1 = document.createElement('span');
-    popupStack1.className = 'modal-stacks';
-    const popupStack2 = document.createElement('span');
-    popupStack2.className = "modal-stacks";
-    const popupStack3 = document.createElement('span');
-    popupStack3.className = "modal-stacks";
-    const div1 = document.createElement('div');
-    div1.className = 'divide';
-    const div2 = document.createElement('div');
-    div2.className = 'illustration works-img';
-    const description = document.createElement('p');
-    description.className = "modal-description description2-text";
-    const popupButton1 = document.createElement('a');
-    popupButton1.className = 'multiposts-btn main-btn';
-    popupButton1.href = '#';
-    const popupButton2 = document.createElement('a');
-    popupButton2.className = 'multiposts-btn main-btn';
-    popupButton2.href = '#';
     const modalChildElements = [
       popupProjectName,
       div1,
@@ -45,7 +104,8 @@ for (let i = 0; i < projectButtonElements.length; i++) {
       div2,
       description,
       popupButton1,
-      popupButton2];
+      popupButton2,
+      div1];
 
     // Set the innerHTML of each child element
     modalChildElements[0].innerHTML = `Project ${i + 1}`;
@@ -56,75 +116,23 @@ for (let i = 0; i < projectButtonElements.length; i++) {
     modalChildElements[8].innerHTML = 'See live';
     modalChildElements[9].innerHTML = 'see source';
 
-
     for (let i = 0; i < projectModalContentElements.length; i++) {
       for (let j = 0; j < modalChildElements.length; j++) {
-        projectModalContentElements[i].appendChild(modalChildElements[j].cloneNode(true));
+        projectModalContentElements[0].appendChild(modalChildElements[j].cloneNode(true));
       }
     }
-    modalContainerElement.style.display = "block";
+    modalContainerElement.style.display = 'block';
   });
+
+  closeModalIconElement.addEventListener('click', () => {
+    modalContainerElement.style.display = 'none';
+    location.reload();
+  });
+
+  window.onclick = function(event) {
+    if (event.target == modalContainerElement) {
+      modalContainerElement.style.display = 'none';
+      location.reload();
+    }
+  };
 }
-
-// Create an array of child elements
-
-// if (window.screen.width >= 768) {
-//   menuIconElement.style.display = 'none';
-// } else menuIconElement.style.display = 'block';
-
-  menuIconElement.addEventListener('click', () => {
-    menuElement.style.display = 'flex';
-    menuIconElement.style.display = 'none';
-    logoElement.style.display = 'none';
-    navElement.style.width = '100%';
-    navElement.style.height = '100vh';
-    navElement.style.paddingTop = '20px';
-    navElement.style.paddingBottom = '75%';
-  });
-  
-  closeMenuElement.addEventListener('click', () => {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-  });
-  
-  menuLinkPortfolioElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
-  
-  menuLinkAboutElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
-  
-  menuLinkContactElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
-
