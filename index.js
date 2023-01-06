04df25dbc92dc2f7c49e321e7a76a6c8b91bd84f
Reggeditt <tkodjosarso@gmail.com>
2023-01-06 17:21:52 +0000

    installed eslint

--- index.js
+++ index.js
@@ -7,79 +7,76 @@ const menuLinkPortfolioElement = document.getElementById('menu-link-portfolio');
 const menuLinkAboutElement = document.getElementById('menu-link-about');
 const menuLinkContactElement = document.getElementById('menu-link-contact');
 
-// if (window.screen.width >= 768) {
-//   menuIconElement.style.display = 'none';
-// } else menuIconElement.style.display = 'block';
+menuIconElement.addEventListener('click', () => {
+  menuElement.style.display = 'flex';
+  menuIconElement.style.display = 'none';
+  logoElement.style.display = 'none';
+  navElement.style.width = '100%';
+  navElement.style.height = '100vh';
+  navElement.style.paddingTop = '20px';
+  navElement.style.paddingBottom = '75%';
+});
 
-  menuIconElement.addEventListener('click', () => {
-    menuElement.style.display = 'flex';
-    menuIconElement.style.display = 'none';
-    logoElement.style.display = 'none';
-    navElement.style.width = '100%';
-    navElement.style.height = '100vh';
-    navElement.style.paddingTop = '20px';
-    navElement.style.paddingBottom = '75%';
-  });
-  
-  closeMenuElement.addEventListener('click', () => {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-  });
-  
-  menuLinkPortfolioElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
-  
-  menuLinkAboutElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
-  
-  menuLinkContactElement.addEventListener('click', () => {
-    if (window.screen.width < 768) {
-    menuElement.style.display = 'none';
-    menuIconElement.style.display = 'inline-block';
-    logoElement.style.display = 'inline-block';
-    navElement.style.width = 'fit-content';
-    navElement.style.height = '18px';
-    navElement.style.paddingTop = '0';
-    navElement.style.paddingBottom = '0';
-    }
-  });
+closeMenuElement.addEventListener('click', () => {
+  menuElement.style.display = 'none';
+  menuIconElement.style.display = 'inline-block';
+  logoElement.style.display = 'inline-block';
+  navElement.style.width = 'fit-content';
+  navElement.style.height = '18px';
+  navElement.style.paddingTop = '0';
+  navElement.style.paddingBottom = '0';
+});
 
-//----------------------- contact validation ---------------------------------
+menuLinkPortfolioElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
+menuLinkAboutElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
+menuLinkContactElement.addEventListener('click', () => {
+  if (window.screen.width < 768) {
+    menuElement.style.display = 'none';
+    menuIconElement.style.display = 'inline-block';
+    logoElement.style.display = 'inline-block';
+    navElement.style.width = 'fit-content';
+    navElement.style.height = '18px';
+    navElement.style.paddingTop = '0';
+    navElement.style.paddingBottom = '0';
+  }
+});
+
+// ----------------------- contact validation ---------------------------------
 const formElement = document.getElementById('contact-form');
-const formNameElement = document.getElementById('name');
+// const formNameElement = document.getElementById('name');
 const formEmailElement = document.getElementById('email');
 
 formElement.addEventListener('submit', (event) => {
   const email = formEmailElement.value;
-  let errmessages = []; 
-  if (email.toLowerCase()!== email) {
+  const errmessages = [];
+  if (email.toLowerCase() !== email) {
     event.preventDefault();
     errmessages.push('email must all be in lowercase letters. form was not submitted');
     const emailErrElement = document.getElementById('email-err');
-    emailErrElement.innerText = errmessages[0]
+    // Destructure the entire `errmessages` array and access the first element using an index
+    const [firstErrorMessage] = errmessages;
+    emailErrElement.innerText = firstErrorMessage;
   }
-
 });
